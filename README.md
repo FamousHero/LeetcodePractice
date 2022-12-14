830aa4ab54d57d8ee326bab2c7c0a7348877f0a7
Daniel Franco <danielfranco2011@gmail.com>
2022-12-13 20:13:52 -0800

    Solve houseRobber

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 |---|-------|----------|
 | 1 | [Two Sum](https://leetcode.com/problems/two-sum/)| [link](./twoSum/)|
 | 931 | [Minimum Falling Path Sum](https://leetcode.com/problems/minimum-falling-path-sum/)| [link](./minimumFallingPathSum/)|
+| 198 | [House Robber](https://leetcode.com/problems/houseRobber/)| [link](./houseRobber/)|
---
+++ houseRobber/houseRobber.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        /**
+         * need to know if its worth more to rob curr house and 2 houses before or skip and keep the money from 
+         * prev house. If we have to keep track of previous answer then its a DP/recursive problem.
+         * For every house check if its worth more to steal from this house and the max of the house we stole
+         * from 2 away or if its worth more to skip the current house and keep the max amount we got from the
+         * previous house. Once we know which is more, update the current houses max to that amount so we know 
+         * what is the best option at this point.
+        */ 
+
+        int ans = 0;
+        if(nums.size() == 1)
+            return nums[0];
+        ans = std::max(nums[0], nums[1]);
+        nums[1] = ans; //updating the houses max amount at this point
+        int prevHouses = nums[0]; 
+        for(int i = 2; i < nums.size(); ++i)
+        {
+            ans = std::max(prevHouses+nums[i], ans); //get the current max as described above
+            nums[i] = ans; //update to new max
+            prevHouses = nums[i-1]; //prevHouses is the same as nums[i-2] when calculating ans
+        }
+        return ans;
+    }
+};
